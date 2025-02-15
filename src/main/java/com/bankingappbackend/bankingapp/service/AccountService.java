ea5ed4f40dfab0b41ec4e76b71c810ac6c50cf32
Rishib#321 <rishitab2209@gmail.com>
2025-02-15 20:24:32 +0530

    service layer

---
+++ src/main/java/com/bankingappbackend/bankingapp/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.bankingappbackend.bankingapp.service;
+
+import com.bankingappbackend.bankingapp.entity.Account;
+
+public interface AccountService {
+
+    Account createAccount(Account);
+
+}
