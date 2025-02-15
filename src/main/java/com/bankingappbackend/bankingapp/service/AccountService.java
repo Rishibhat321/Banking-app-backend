e154b8dccb6c8dde73439501f3c997c430307eef
Rishib#321 <rishitab2209@gmail.com>
2025-02-15 21:44:56 +0530

    Created methods in service layer

--- src/main/java/com/bankingappbackend/bankingapp/service/AccountService.java
+++ src/main/java/com/bankingappbackend/bankingapp/service/AccountService.java
@@ -1,6 +1,8 @@
 package com.bankingappbackend.bankingapp.service;
 
 import com.bankingappbackend.bankingapp.dto.AccountDto;
+
+import java.util.List;
 // import com.bankingappbackend.bankingapp.entity.Account;
 
 public interface AccountService {
@@ -10,5 +12,9 @@ public interface AccountService {
     AccountDto getAccountById(Long id);
 
     AccountDto deposit(Long id, double amount);
+
+    AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
 
 }
