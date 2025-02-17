fd24cc9781a59a494c3f1123e0b4f989fee3b1a8
Rishib#321 <rishitab2209@gmail.com>
2025-02-17 20:17:39 +0530

    created record for transferring funds

---
+++ src/main/java/com/bankingappbackend/bankingapp/dto/TransferFundDto.java
@@ -0,0 +1,7 @@
+package com.bankingappbackend.bankingapp.dto;
+
+public record TransferFundDto(Long fromAccountId,
+                              Long toAccountId,
+                              double amount) {
+
+}
