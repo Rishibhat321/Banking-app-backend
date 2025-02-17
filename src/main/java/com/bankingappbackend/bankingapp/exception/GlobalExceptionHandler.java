5454f25a7d803c2c947eace8f920f810c7f085de
Rishib#321 <rishitab2209@gmail.com>
2025-02-17 19:38:17 +0530

    Added exceptions for global exception handling and generic exceptions

---
+++ src/main/java/com/bankingappbackend/bankingapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.bankingappbackend.bankingapp.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
