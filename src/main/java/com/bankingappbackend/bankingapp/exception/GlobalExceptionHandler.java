965d6726e76c5371df4dfb43f8773dbd83f758ba
Rishib#321 <rishitab2209@gmail.com>
2025-02-17 19:42:48 +0530

    Added exceptions for global exception handling and generic exceptions

--- src/main/java/com/bankingappbackend/bankingapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bankingappbackend/bankingapp/exception/GlobalExceptionHandler.java
@@ -1,10 +1,33 @@
 package com.bankingappbackend.bankingapp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     // instead of writing the exception handler code for each controller, create a global exception handler
+
+    // handle specific exception - Account Exception
+
+    @ExceptionHandler(AccountException.class)
+    public ResponseEntity<ErrorDetails> handleAccountException(AccountException exception,
+                                                               WebRequest webRequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "ACCOUNT_NOT FOUND"
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
 
 }
