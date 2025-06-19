ec599dc1873c8b3186d604849fcba341de4b2aaa
Rishib#321 <rishitab2209@gmail.com>
2025-06-19 20:30:13 +0530

    implemented exception handling and global handling

--- src/main/java/com/bankingappbackend/bankingapp/exception/AccountException.java
+++ src/main/java/com/bankingappbackend/bankingapp/exception/AccountException.java
@@ -2,7 +2,7 @@ package com.bankingappbackend.bankingapp.exception;
 
 public class AccountException extends RuntimeException{
 
-    public AccountException(String message) {
+    public AccountException(String message) {   // this message holds the error message
         // super class constructor
         super(message);
     }
